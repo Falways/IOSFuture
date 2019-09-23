5d22aeac2fa52546dc528db33e3d962421a1581c
徐航 <xuhang@xuhangs-MacBook-Pro.local>
2019-09-23 10:59:12 +0800

    init

---
+++ ios_future/ios_future/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_future
+//
+//  Created by 徐航 on 2019/9/23.
+//  Copyright © 2019 falways. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
